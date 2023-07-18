c78e6e24c11d2b3bdba2053e7d9bf66df0efe60f
Young Joon Lee <entelecheia@hotmail.com>
2023-07-18 17:50:58 +0900

    fix(corprep): introduce setLogger for HyFI

--- src/corprep/__init__.py
+++ src/corprep/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from hyfi import HyFI, about, global_config
+from hyfi import HyFI, about, global_config  # type: ignore
 
 from ._version import __version__
 
@@ -9,6 +9,8 @@ __package_name__ = os.path.basename(os.path.dirname(__file__))
 
 # Extract package information
 about.__package_name__ = __package_name__
+
+HyFI.setLogger()
 
 
 def get_version() -> str:
