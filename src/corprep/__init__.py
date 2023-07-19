446e36a36be0f0ea390ba504ea7cdd589096afea
Young Joon Lee <entelecheia@hotmail.com>
2023-07-20 08:44:40 +0900

    refactor(corprep): add type hinting and documentation.

--- src/corprep/__init__.py
+++ src/corprep/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from hyfi import HyFI, about, global_config  # type: ignore
 
-from ._version import __version__
+from ._version import __version__  # type: ignore
 
 # Read the package name from the current directory
 __package_name__ = os.path.basename(os.path.dirname(__file__))
@@ -10,8 +10,10 @@ __package_name__ = os.path.basename(os.path.dirname(__file__))
 # Extract package information
 about.__package_name__ = __package_name__
 
+# Initialize the logger
 HyFI.setLogger()
 
 
 def get_version() -> str:
+    """Get the package version."""
     return __version__
