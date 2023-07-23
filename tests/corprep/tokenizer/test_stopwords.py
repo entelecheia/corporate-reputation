a17d48120b628eb8bbaa47b6aba9bc6a50de44ae
Young Joon Lee <entelecheia@hotmail.com>
2023-07-24 04:58:22 +0900

    test: add english language support to nltk_stopwords config in Stopwords test

--- tests/corprep/tokenizer/test_stopwords.py
+++ tests/corprep/tokenizer/test_stopwords.py
@@ -5,6 +5,7 @@ from corprep.tokenizer.stopwords import Stopwords
 def test_stopwords():
     print(HyFI.get_caller_module_name())
     cfg = HyFI.compose_as_dict("stopwords")
+    cfg["nltk_stopwords_lang"] = "english"
     stop = Stopwords(**cfg)
     print(stop.stopwords_list)
     assert len(stop.stopwords_list) == 179
