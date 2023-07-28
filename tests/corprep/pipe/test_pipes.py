67cfeecfd061dd2ee8d4008f9fc56fa3b384d172
Young Joon Lee <entelecheia@hotmail.com>
2023-07-28 16:18:39 +0900

    test(test_pipes): add new test cases for pipes in corprep module

---
+++ tests/corprep/pipe/test_pipes.py
@@ -0,0 +1,14 @@
+from corprep.datasets.io import load_raw_dataset
+from corprep.datasets.similarity import find_similar_docs_ac
+from corprep.absa.agent import absa_agent_predict
+from corprep import HyFI
+
+
+def test_pipes():
+    HyFI.save_hyfi_pipe_config(load_raw_dataset)
+    HyFI.save_hyfi_pipe_config(find_similar_docs_ac)
+    HyFI.save_hyfi_pipe_config(absa_agent_predict)
+
+
+if __name__ == "__main__":
+    test_pipes()
