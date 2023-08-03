88d631f1387fe78db97c202765a1203d980f50d2
Young Joon Lee <entelecheia@hotmail.com>
2023-08-03 13:45:34 +0900

    refactor(test_pipes): remove absa_agent_predict from test_pipes

--- tests/corprep/pipe/test_pipes.py
+++ tests/corprep/pipe/test_pipes.py
@@ -1,13 +1,9 @@
-from corprep.datasets.io import load_raw_dataset
 from corprep.datasets.similarity import find_similar_docs_ac
-from corprep.absa.agent import absa_agent_predict
 from corprep import HyFI
 
 
 def test_pipes():
-    HyFI.generate_config(load_raw_dataset, use_pipe_obj=False)
     HyFI.generate_config(find_similar_docs_ac, use_first_arg_as_pipe_obj=True)
-    HyFI.generate_config(absa_agent_predict, use_first_arg_as_pipe_obj=True)
 
 
 if __name__ == "__main__":
