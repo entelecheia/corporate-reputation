80428c1567911c289778c9699af1c0799a367a2e
Young Joon Lee <entelecheia@hotmail.com>
2023-08-03 11:50:15 +0900

    refactor(test_pipes): update HyFI configurations to use generate_config method

--- tests/corprep/pipe/test_pipes.py
+++ tests/corprep/pipe/test_pipes.py
@@ -5,9 +5,9 @@ from corprep import HyFI
 
 
 def test_pipes():
-    HyFI.save_hyfi_pipe_config(load_raw_dataset, use_pipe_obj=False)
-    HyFI.save_hyfi_pipe_config(find_similar_docs_ac)
-    HyFI.save_hyfi_pipe_config(absa_agent_predict)
+    HyFI.generate_config(load_raw_dataset, use_pipe_obj=False)
+    HyFI.generate_config(find_similar_docs_ac, use_first_arg_as_pipe_obj=True)
+    HyFI.generate_config(absa_agent_predict, use_first_arg_as_pipe_obj=True)
 
 
 if __name__ == "__main__":
