641fabd6159e1ba735481e944a5e661422c98c58
Young Joon Lee <entelecheia@hotmail.com>
2023-08-03 16:31:12 +0900

    test(test_pipes.py): add filter_dataset to test_pipes

--- tests/corprep/pipe/test_pipes.py
+++ tests/corprep/pipe/test_pipes.py
@@ -1,9 +1,17 @@
 from corprep.datasets.similarity import find_similar_docs_ac
+from corprep.datasets.filter import filter_dataset
 from corprep import HyFI
+from hyfi.composer import PipeTargetTypes
 
 
 def test_pipes():
-    HyFI.generate_config(find_similar_docs_ac, use_first_arg_as_pipe_obj=True)
+    HyFI.generate_pipe_config(
+        find_similar_docs_ac, pipe_target_type=PipeTargetTypes.GENERAL_EXTERNAL_FUNCS
+    )
+    HyFI.generate_pipe_config(
+        filter_dataset,
+        pipe_target_type=PipeTargetTypes.GENERAL_EXTERNAL_FUNCS,
+    )
 
 
 if __name__ == "__main__":
